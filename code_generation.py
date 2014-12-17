0b94cf2e24eb8f2a31a5ae282bbde0d5ae316476
Milo Spencer-Harper <miloharper@gmail.com>
2014-12-17 13:33:37 +0000

    Creates multiple lines of code.

--- code_generation.py
+++ code_generation.py
@@ -4,20 +4,21 @@ import random
 def create_code():
     code = []
     available_commands = ["if on_food_square():", "if not on_food_square():", "up()", "down()", "left()", "right()"]
-    random_number = random.randrange(0, len(available_commands))
-    indents = get_current_number_of_indents(code)
-    if last_command_is_pass(code):
-        # Replace pass with new command
-        code[-1] = whitespace(indents) + available_commands[random_number]
-    else:
-        # Either add the command within the current block, or outside it by using one less indent
-        if with_probability(50):
-            indents = max(0, indents - 1)
-        code.append(whitespace(indents) + available_commands[random_number])
+    for _ in range(20):
+        random_number = random.randrange(0, len(available_commands))
+        indents = get_current_number_of_indents(code)
+        if last_command_is_pass(code):
+            # Replace pass with new command
+            code[-1] = whitespace(indents) + available_commands[random_number]
+        else:
+            # Either add the command within the current block, or outside it by using one less indent
+            if with_probability(50):
+                indents = max(0, indents - 1)
+            code.append(whitespace(indents) + available_commands[random_number])
 
-    # If an 'if' command was added, also add a 'pass' to make it valid Python
-    if random_number == 0 or random_number == 1:
-        code.append(whitespace(indents + 1) + "pass")
+        # If an 'if' command was added, also add a 'pass' to make it valid Python
+        if random_number == 0 or random_number == 1:
+            code.append(whitespace(indents + 1) + "pass")
     return code
 
 
