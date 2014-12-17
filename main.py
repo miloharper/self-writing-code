c88ea6f29f31a3da87abe4fccf2f3ab482c5f46c
Milo Spencer-Harper <miloharper@gmail.com>
2014-12-17 16:13:21 +0000

    Improved clarity of console output.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
     for _ in range(50):
         food_maps.append(create_random_food_map());
     results = [] # Contains tuples of each mutation along with how long mutation survived
-    #Run each mutation with ten random food maps and then take it's average healthtime.
+    #Run each mutation with ten random food maps and then take it's average survival time.
     for code_mutation in code_mutations:
         current_mutation = convert_code_from_list_to_string(code_mutation)
         total_moves = 0 
@@ -34,12 +34,10 @@ def main():
             health = 100
             food_map = random.choice(food_maps)
             exec current_mutation
-    
         results.append((current_mutation, (total_moves / 10)) ) # Take the average 
         total_moves = 0
     sorted(results, key=lambda result: result[1], reverse=True) #sort by length of health
-    print "The top mutation was as follows: \n" + results[0][0] + " which lasted for " + str(results[0][1])
-
+    print "The top mutation was as follows: \n" + results[0][0] + "\n\n Which on average survived for " + str(results[0][1]) + " units of time."
 
 
 def convert_code_from_list_to_string(input_code):
