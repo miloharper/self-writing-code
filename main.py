751b5681c332a91accf46be8b5204abee537bbb6
Milo Spencer-Harper <miloharper@gmail.com>
2015-06-21 23:58:58 +0800

    Updates console every 100 code mutations, so the user can see the progress.

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ def main():
     code_mutations = create_code_mutations(10000)
     food_maps = create_food_maps(100)
     results = []  # Contains tuples of each mutation along with how long mutation survived
+    mutation_number = 0
+    last_update = 0
     for code_mutation in code_mutations:
         current_mutation = convert_code_from_list_to_string(code_mutation)
         brain_size = len(current_mutation)
         average_survival_time = measure_average_survival_time(current_mutation, food_maps, number_of_tests)
         results.append((current_mutation, average_survival_time, brain_size))
+        mutation_number += 1
+        if mutation_number == last_update + 100:
+            last_update = mutation_number
+            print "Mutation number " + str(mutation_number)
     sorted(results, key=lambda result: result[1], reverse=True)  # Rank the best average survival times
     print "The top mutation was as follows: \n" + results[0][0] + "\n\nWhich on average survived for " + str(
         results[0][1]) + " units of time and had a brain size of " + str(results[0][2]) + " characters of code."
